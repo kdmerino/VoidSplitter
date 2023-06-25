beb4a52a556005f4303665313043468771c8c245
Kevin Merino Castaneda <kmerino.castaneda@gmail.com>
2023-06-25 03:40:35 -0700

    sync release with development:latest

--- cpp/src/hello-world-v4.cc
+++ cpp/src/hello-world-v4.cc
@@ -15,18 +15,27 @@ ABSL_FLAG(std::string, who, "world", "Greeting message name");
 
 // Main
 int main(int argc, char** argv) {
+  // Verify that the version of the library that we linked against is
+  //   // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
   absl::ParseCommandLine(argc, argv);
   std::string who = absl::GetFlag(FLAGS_who);
+
   cpp::HelloWorldRequest request;
   request.set_name(who);
+
   std::cout << "Hello " << request.name() << std::endl;  
   print_localtime();
   std::cout << std::endl;
 
-  std::vector<std::string> v = {"foo", "bar", "baz"};
+  std::vector<std::string> v = {"sample", "vector", "entry"};
   std::string s = absl::StrJoin(v, "-");
 
   std::cout << "Joined string: " << s << std::endl;
+
+  // Delete all global objects allocated by libprotobuf
+  google::protobuf::ShutdownProtobufLibrary();
 
   return 0;
 }
