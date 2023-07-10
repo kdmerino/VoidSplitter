38381d23d38ba2c4e80e3295ba88606e66c8d198
kdmerino <kmerino.castaneda@gmail.com>
2023-07-09 22:28:32 -0700

    rebase

--- cpp/src/hello-world-v2.cc
+++ cpp/src/hello-world-v2.cc
@@ -1,12 +1,8 @@
-#include "hello-greet.h"
 #include <ctime>
 #include <string>
 #include <iostream>
-
-void print_localtime() {
-    std::time_t result = std::time(nullptr);
-    std::cout << std::asctime(std::localtime(&result));
-}
+#include "hello-greet.h"
+#include "cpp/lib/hello-time.h"
 
 int main(int argc, char** argv) {
   std::string who = "world";
--- cpp/src/hello-world-v3.cc
+++ cpp/src/hello-world-v3.cc
@@ -1,15 +1,42 @@
 #include <ctime>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 #include "hello-greet.h"
 #include "cpp/lib/hello-time.h"
+#include "cpp/src/hello-world-request.pb.h"
 
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/strings/str_join.h"
+
+// Flags
+ABSL_FLAG(std::string, who, "world", "Greeting message name");
+
+// Main
 int main(int argc, char** argv) {
-  std::string who = "world";
-  if (argc > 1) who = argv[1];
-  std::cout << get_greet(who) << std::endl;
+  // Verify that the version of the library that we linked against is
+  //   // compatible with the version of the headers we compiled against.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  absl::ParseCommandLine(argc, argv);
+  std::string who = absl::GetFlag(FLAGS_who);
+
+  cpp::HelloWorldRequest request;
+  request.set_name(who);
+
+  std::cout << "Hello " << request.name() << std::endl;  
   print_localtime();
   std::cout << std::endl;
+
+  std::vector<std::string> v = {"sample", "vector", "entry"};
+  std::string s = absl::StrJoin(v, "-");
+
+  std::cout << "Joined string: " << s << std::endl;
+
+  // Delete all global objects allocated by libprotobuf
+  google::protobuf::ShutdownProtobufLibrary();
+
   return 0;
 }
 
